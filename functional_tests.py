f6b06d10041a268583ec46a04bd91c0fe220c3ea
jaiba <webbusinessdev@gmail.com>
2014-08-16 23:15:07 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,17 +18,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		# I should see the page title reinforce that I'm looking at a to-do list
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('hi').text
+		self.assertIn('To-Do', header_text)
 
 		# I'm invited to enter a to-do item at the home page
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+						inputbox.get_attribute('placeholder'),
+						'Enter a to-do item'
+		)
 
 		# I can type "Learn tdd" into a text box
+		inputbox.send_keys('Learn tdd')
 
 		# After hitting enter, the page updates and now it shows
 		# "1: Learn tdd" as an item on a to-do list
-
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+						any(row.text == '1: Learn tdd' for row in rows)
+		)
 		# There is still a text box in which to enter more to-dos.
 		# I enter "Build bead stringing app"
+		self.fail('Finish the test!')
 
 		# The page updates again and now shows two items on my list
 
