4de9d1d848ba1ff6c6992c4c4d843c4198ff7f4a
jaiba <webbusinessdev@gmail.com>
2014-11-03 14:07:28 -0500

    make functional tests an app separate from potential live server - introduce LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
   def test_can_start_a_list_and_retrieve_it_later(self):
 
     # I'm making a to-do list app to motivate myself. Here's where I go to look at it.
-    self.browser.get('http://localhost:8000')
+    self.browser.get(self.live_server_url)
 
     # I should see the page title reinforce that I'm looking at a to-do list
     self.assertIn('To-Do', self.browser.title)
@@ -57,6 +57,4 @@ class NewVisitorTest(unittest.TestCase):
     # the list because the site offers some text explaining that
     self.fail('Finish the test!')
     # I visit that URL and see my list
-
-if __name__ == '__main__':
-  unittest.main(warnings = 'ignore')
+    
